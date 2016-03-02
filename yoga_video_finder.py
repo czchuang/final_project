f04b83efdfb4712314415c14e5b296ad98f88261
lauramatchett <laura.m.matchett@gmail.com>
2016-03-01 19:53:24 -0800

    ignored youtube api call file

---
+++ yoga_video_finder.py
@@ -0,0 +1,17 @@
+#welcome message
+print "Welcome to the yoga video finder! Are you ready to find your next online yoga class?"
+
+#determine yoga type
+
+yoga_type = raw_input ("What kind of yoga are you interested in? Please select from: Hatha, Vinyasa, Iyengar, Bikram, Kundalini, or Ashtanga.")
+
+#determine video length 
+
+video_length = raw_input ("What long would you like your video to be? Please select from (in minutes): 15, 30, 45, 60.")
+
+#determine video difficulty 
+
+yoga_level = raw_input ("How difficult would you like the video to be? Please select from: Easy, Moderate, Hard.")
+
+#call YouTube API
+
