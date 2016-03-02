190a20d1245c5bd14f342581cdf60c551ab1537c
lauramatchett <laura.m.matchett@gmail.com>
2016-03-01 20:46:16 -0800

    added API call

--- yoga_video_finder.py
+++ yoga_video_finder.py
@@ -15,3 +15,36 @@ yoga_level = raw_input ("How difficult would you like the video to be? Please se
 
 #call YouTube API
 
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+
+with open("api_keys.py") as youtube_key_file:
+	DEVELOPER_KEY = youtube_key_file.read()
+	YOUTUBE_API_SERVICE_NAME = "youtube"
+	YOUTUBE_API_VERSION = "v3"
+
+	def youtube_search(query, max_results=3):
+  		
+  		youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+
+  		search_response = youtube.search().list(
+   			q=query,
+    		part="id,snippet",
+    		maxResults=max_results
+  		).execute()
+
+    	result_transform = lambda search_result: {
+    							'id': search_result['id']['videoId'],
+    							'title': search_result['snippet']['title'],
+    							'thumbnail': search_result['snippet']['thumbnails']['defaault'],
+    							'data': search_result['snippet']['publishedAt']
+    						}	
+
+    	result_filter = lambda search_result: search_result['id']['kind'] == 'youtube#video'
+
+    	return map(result_transform, filter(result_filter, search_response,get("items", [])))
+
+
+
+#http://www.programmableweb.com/news/how-to-query-youtube-through-its-python-apis/how-to/2014/03/26
